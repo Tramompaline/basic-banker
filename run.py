fa51b8116370a1c9d701b28e43f2073739ceb0c5
Robert McSweeney <robmcsweeney88@gmail.com>
2024-08-21 20:59:22 +0000

    Add input function to select a menu option

--- run.py
+++ run.py
@@ -14,7 +14,10 @@ is_running = True
 
 while is_running:
     print("Welcome To Python Bank!")
-    print("1.Show Balance")
-    print("2.Deposit")
-    print("3.Withdraw")
-    print("4.Exit")
+    print("1. Show Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    choice = input("Please select an option (1-4): ")
+
